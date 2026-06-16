0345f56208312c78c6d37a17f464522d72a30cd9
zhangdawei <zhangdawei123>
2026-06-16 14:55:23 +0800

    提交

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -245,7 +245,7 @@ const Sidebar: React.FC = () => {
             IT设备资产管理系统
           </Text>
           <Text size="sm" fw={500}>
-            v0.0.4
+            v0.0.5
           </Text>
         </Box>
       </ScrollArea>
